ca9e42cfd380e363101943ed193cadb59b7960fd
hongzhi1234 <zhanghongzhi@freezonex.io>
2024-05-16 10:39:51 +0800

    check in modified sql file

--- src/main/resources/sql/wms_warehouse.sql
+++ src/main/resources/sql/wms_warehouse.sql
@@ -1,33 +1,33 @@
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(1, 'WH_001', 'Warehouse 1', 'Type B', 'Manager 1', 'Department 1', 'email1@example.com', 'Project Group 1', 'Note about Warehouse 1', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(1, 'WH_001', 'Warehouse 1', 'Type B', 'Manager 1', 'Department 1', 'email1@example.com', 'Project Group 1', 'Note about Warehouse 1', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(2, 'WH_002', 'Warehouse 2', 'Type A', 'Manager 2', 'Department 2', 'email2@example.com', 'Project Group 2', 'Note about Warehouse 2', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(2, 'WH_002', 'Warehouse 2', 'Type A', 'Manager 2', 'Department 2', 'email2@example.com', 'Project Group 2', 'Note about Warehouse 2', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(3, 'WH_003', 'Warehouse 3', 'Type B', 'Manager 3', 'Department 3', 'email3@example.com', 'Project Group 3', 'Note about Warehouse 3', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(3, 'WH_003', 'Warehouse 3', 'Type B', 'Manager 3', 'Department 3', 'email3@example.com', 'Project Group 3', 'Note about Warehouse 3', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(4, 'WH_004', 'Warehouse 4', 'Type A', 'Manager 4', 'Department 4', 'email4@example.com', 'Project Group 4', 'Note about Warehouse 4', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(4, 'WH_004', 'Warehouse 4', 'Type A', 'Manager 4', 'Department 4', 'email4@example.com', 'Project Group 4', 'Note about Warehouse 4', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(5, 'WH_005', 'Warehouse 5', 'Type B', 'Manager 5', 'Department 5', 'email5@example.com', 'Project Group 5', 'Note about Warehouse 5', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(5, 'WH_005', 'Warehouse 5', 'Type B', 'Manager 5', 'Department 5', 'email5@example.com', 'Project Group 5', 'Note about Warehouse 5', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(6, 'WH_006', 'Warehouse 6', 'Type A', 'Manager 6', 'Department 6', 'email6@example.com', 'Project Group 6', 'Note about Warehouse 6', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(6, 'WH_006', 'Warehouse 6', 'Type A', 'Manager 6', 'Department 6', 'email6@example.com', 'Project Group 6', 'Note about Warehouse 6', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(7, 'WH_007', 'Warehouse 7', 'Type B', 'Manager 7', 'Department 7', 'email7@example.com', 'Project Group 7', 'Note about Warehouse 7', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(7, 'WH_007', 'Warehouse 7', 'Type B', 'Manager 7', 'Department 7', 'email7@example.com', 'Project Group 7', 'Note about Warehouse 7', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(8, 'WH_008', 'Warehouse 8', 'Type A', 'Manager 8', 'Department 8', 'email8@example.com', 'Project Group 8', 'Note about Warehouse 8', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(8, 'WH_008', 'Warehouse 8', 'Type A', 'Manager 8', 'Department 8', 'email8@example.com', 'Project Group 8', 'Note about Warehouse 8', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(9, 'WH_009', 'Warehouse 9', 'Type B', 'Manager 9', 'Department 9', 'email9@example.com', 'Project Group 9', 'Note about Warehouse 9', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(9, 'WH_009', 'Warehouse 9', 'Type B', 'Manager 9', 'Department 9', 'email9@example.com', 'Project Group 9', 'Note about Warehouse 9', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(10, 'WH_010', 'Warehouse 10', 'Type A', 'Manager 10', 'Department 10', 'email10@example.com', 'Project Group 10', 'Note about Warehouse 10', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(10, 'WH_010', 'Warehouse 10', 'Type A', 'Manager 10', 'Department 10', 'email10@example.com', 'Project Group 10', 'Note about Warehouse 10', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
 INSERT INTO wms_warehouse
-(id, warehouse_id, name, `type`, manager, department, email, project_group, note, del_flag, create_time, update_time)
-VALUES(11, 'WH_011', 'Warehouse 11', 'Type A', 'Manager 11', 'Department 11', 'email10@example.com', 'Project Group 11', 'Note about Warehouse 11', NULL, '2024-03-22 06:07:34', '2024-03-22 06:10:59');
+(id, warehouse_id, name, `type`, manager, department, email, project_group, note, create_time, update_time)
+VALUES(11, 'WH_011', 'Warehouse 11', 'Type A', 'Manager 11', 'Department 11', 'email10@example.com', 'Project Group 11', 'Note about Warehouse 11', '2024-03-22 06:07:34', '2024-03-22 06:10:59');
